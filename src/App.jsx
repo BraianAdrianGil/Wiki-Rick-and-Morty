ead0dd3d76c0e711d8130864d0f528ee122fcf43
BraianAdrianGil <braian.adrian.gagliardo@gmail.com>
2023-06-02 07:45:44 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="header__image__container">
-        <img src="../public/rickandmorty.png" alt="" />
-      </div>
+      <div className="header__image__container"></div>
       {location ? <Location location={location} /> : <Loader />}
       <Input loadLocation={loadLocation} />
       {location && location.residents.length > 0 ? (
